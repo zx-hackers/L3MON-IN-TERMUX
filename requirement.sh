4804d3495b324614ae232c5df4ed23ed2ebef7b3
shailesh-zx <138345328+zx-hackers@users.noreply.github.com>
2023-09-20 06:58:07 +0530

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -1,7 +1,7 @@
 apt install nodejs
-npm install
-npm install
-npm install
-npm install
+npm install lowdb
+npm install express
+npm install socket.io
+npm install node-geoip
 dpkg -i lemon.deb
 npm audit fix --force
